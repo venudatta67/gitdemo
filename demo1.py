0abab15816879f3c77455f74f7b3f0f8ebaa68ca
venudatta <venukarumuri986@gmail.com>
2025-01-22 10:55:57 +0530

    2nd version of even/odd function.

--- demo1.py
+++ demo1.py
@@ -1,8 +1,11 @@
 def even_odd(number):
-    if number % 2 == 0:
-        print("even")
+    if number != 0:
+        if number % 2 == 0:
+            print("even")
+        else:
+            print("odd")
     else:
-        print("odd")
+        print("zero is not even or odd")
 
 if __name__=="__main__":
     even_odd(6)
